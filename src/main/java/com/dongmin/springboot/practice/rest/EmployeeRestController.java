30ea7aa702023ec7ccf50417d81469310af19464
Srltas <kdm97912@gmail.com>
2022-03-31 01:53:23 +0900

    Hibernate로 REST CRUD API 빌드

--- src/main/java/com/dongmin/springboot/practice/rest/EmployeeRestController.java
+++ src/main/java/com/dongmin/springboot/practice/rest/EmployeeRestController.java
@@ -3,9 +3,11 @@ package com.dongmin.springboot.practice.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -57,5 +59,33 @@ public class EmployeeRestController {
 		employeeService.save(theEmployee);
 		
 		return theEmployee;
+	}
+	
+	// add mapping for PUT /empolyee - update existing employee
+	
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee theEmployee) {
+		
+		employeeService.save(theEmployee);
+		
+		return theEmployee;
+	}
+	
+	// add mapping for DELETE /employee/{employeeId} - delete employee
+	
+	@DeleteMapping("/employees/{employeeId}")
+	public String deleteEmployee(@PathVariable int employeeId) {
+		
+		Employee tempEmployee = employeeService.findById(employeeId);
+		
+		// throw exception if null
+		
+		if (tempEmployee == null) {
+			throw new RuntimeException("Exployee id not found - " + employeeId);
+		}
+		
+		employeeService.deleteById(employeeId);
+		
+		return "Delete employee id - " + employeeId;
 	}
 }
